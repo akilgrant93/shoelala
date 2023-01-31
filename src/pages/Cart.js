8d7d919fe5880082f9f720cddef56ce8f78f3c97
Akil Grant <akil.grant.93@gmail.com>
2023-01-30 22:41:00 -0500

    fixed cart exit button

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,16 +1,25 @@
 import React, {useState} from 'react'
+import CartItem from './CartItem'
 
 export default function Cart(props) {
-  const [ cartItems, setCartItems ] = useState([])
+  const [ cartItems, setCartItems ] = useState([{category:'Air Jordan',price:420,image:'https://cdn.flightclub.com/750/TEMPLATE/011834/1.jpg?w=360',title:'Air Jordan 11 retro "gamma blue"'}])
   const [ subtotal, setSubtotal ] = useState('0.00')
 
   return (
     <div style={{zIndex:99, backgroundColor:'rgba(0,0,0,.5)', width: '100%', height: '100%', display:'flex', justifyContent:'center', alignItems:'center'}} className='fixed top-12'>
       <div className='p-10 bg-white w-80 h-3/5 rounded-xl flex flex-col justify-between shadow-lg'>
+
       <p className='text-sm font-bold pb-2' style={{borderBottomWidth: 1, borderBottomColor: 'black',}}>CART</p>
-    <p className='absolute py-1 px-3  rounded-lg bg-white shadow-lg cursor-pointer' style={{marginTop:'-3.75%', marginLeft:'19.75%'}} onClick={() => {
+      {cartItems.length > 0 ? cartItems.map((item, idx) => {
+        return <CartItem key={idx} item={item}/>
+      }) : <p className='text-center'>Cart empty</p> }
+
+    <p className='absolute py-1 px-3  rounded-lg bg-slate-100 shadow-lg cursor-pointer'
+    style={{marginTop:-47.5, marginLeft:255}}
+    onClick={() => {
       props.setIsOpen(false)
     }}>X</p>
+
     <div>
       <div style={{borderTopWidth: 1, borderTopColor: 'black',}} className='flex justify-between'>
     <p className='text-sm font-bold py-2'>SUBTOTAL</p>
---
+++ src/pages/CartItem.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CartItem(props) {
+
+  return (
+    <div>CartItem</div>
+  )
+}
