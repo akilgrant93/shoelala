2bd218e7bbbff70a6cefaf0ebeb5c607274d02f5
Akil Grant <akil.grant.93@gmail.com>
2023-01-30 23:17:53 -0500

    added item quantity control but needs work - likely object structure incoming to replace cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,7 +3,14 @@ import CartItem from './CartItem'
 
 export default function Cart(props) {
   const [ cartItems, setCartItems ] = useState([{category:'Air Jordan',price:420,image:'https://cdn.flightclub.com/750/TEMPLATE/011834/1.jpg?w=360',title:'Air Jordan 11 retro "gamma blue"'}])
-  const [ subtotal, setSubtotal ] = useState('0.00')
+  const [ subtotal, setSubtotal ] = useState(0)
+  const [ quantity, setQuantity ] = useState(1)
+
+  const changeQuantity = (e, data) => {
+    console.log(e)
+    console.log(data)
+    setQuantity(e.target.value)
+  }
 
   return (
     <div style={{zIndex:99, backgroundColor:'rgba(0,0,0,.5)', width: '100%', height: '100%', display:'flex', justifyContent:'center', alignItems:'center'}} className='fixed top-12'>
@@ -11,7 +18,7 @@ export default function Cart(props) {
 
       <p className='text-sm font-bold pb-2' style={{borderBottomWidth: 1, borderBottomColor: 'black',}}>CART</p>
       {cartItems.length > 0 ? cartItems.map((item, idx) => {
-        return <CartItem key={idx} item={item}/>
+        return <CartItem changeQuantity={changeQuantity} quantity={quantity} key={idx} item={item}/>
       }) : <p className='text-center'>Cart empty</p> }
 
     <p className='absolute py-1 px-3  rounded-lg bg-slate-100 shadow-lg cursor-pointer'
@@ -23,7 +30,7 @@ export default function Cart(props) {
     <div>
       <div style={{borderTopWidth: 1, borderTopColor: 'black',}} className='flex justify-between'>
     <p className='text-sm font-bold py-2'>SUBTOTAL</p>
-    <p className='text-sm font-bold py-2'>{subtotal}</p>
+    <p className='text-sm font-bold py-2'>${subtotal}</p>
       </div>
     <p style={{fontSize:10, textAlign:'end'}}>Tax included. Shipping calculated at checkout</p>
     </div>
--- src/pages/CartItem.js
+++ src/pages/CartItem.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 export default function CartItem(props) {
-
   return (
-    <div>CartItem</div>
+    <div>
+      <div className='flex justify-between'>
+      <img className='w-1/4 h-auto' src={props.item.image} alt={props.item.title}/>
+      <p className='text-end text-xs pl-10'>{props.item.title}</p>
+      </div>
+      <div className='flex justify-end'>
+      <input
+      className='bg-slate-100 w-12 rounded-sm ml-2 px-2 h-5 text-xs'
+      type={'number'}
+      min={0}
+      value={props.quantity}
+      onChange={(e) => {props.changeQuantity(e, props.item)}}
+      name='quantity'/>
+      <p className='text-end text-xs ml-2'>${props.quantity*props.item.price}</p>
+      </div>
+    </div>
   )
 }
