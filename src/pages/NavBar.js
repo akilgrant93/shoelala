1a30224e7f9e1aa6af513084242ccd78c24210cf
Akil Grant <akil.grant.93@gmail.com>
2023-02-01 15:06:05 -0500

    added logo

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -8,14 +8,16 @@ export default function NavBar() {
 
   return (
     <div>
-    <ul className='shadow-md py-2 justify-end pr-10 items-center' style={{position: 'fixed', top: 0, width: '100%', zIndex: 999, display:'flex', backgroundColor:'white'}}>
-      <li className='font-semibold' style={{padding:5, marginRight: '25%'}}><Link href="/">SHOELALA</Link></li>
+    <ul className='shadow-md py-2 justify-end pr-10 items-center' style={{position: 'fixed', top: 0, width: '100%', zIndex: 999, display:'flex', justifyContent:'space-between' ,backgroundColor:'white'}}>
+      <li className='font-semibold mx-10' style={{padding:5}}><Link href="/"><img className='max-h-8' src={'/shoelala.png'} alt={'Shoelala logo'}/></Link></li>
+      <div className='flex'>
       <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/">ALL</Link></li>
       <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/jordan"><SiJordan className='mx-1' size={18}/></Link></li>
       <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/adidas"><SiAdidas className='mx-1' size={18}/></Link></li>
       <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/nike"><SiNike className='mx-1' size={18}/></Link></li>
       <li className='font-semibold cursor-pointer' onClick={() => {setIsOpen(true)}} style={{padding:5}}><AiOutlineShoppingCart className='mx-1' size={18}/></li>
       <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/account">ACCOUNT</Link></li>
+      </div>
     </ul>
       {isOpen && <Cart setIsOpen={setIsOpen} />}
     </div>
