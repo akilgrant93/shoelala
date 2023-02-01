e95490a4fa34466e825a946b3ae9fe52ef0505b1
Akil Grant <akil.grant.93@gmail.com>
2023-02-01 18:25:25 -0500

    tailwind in cartItem converted to raw jsx css styles

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,20 +2,21 @@ import React from 'react'
 
 export default function CartItem(props) {
   return (
-    <div className={['border-b py-2']}>
-      <div className='flex justify-between'>
-        <img className='w-1/4 h-auto' src={props.item.image} alt={props.item.title}/>
-      <p className='text-end text-xs pl-10'>{props.item.title}</p>
+    <div style={{paddingBottom: '.5rem', paddingTop: '.5rem', borderBottomWidth: 1}}>
+      <div style={{display:'flex', justifyContent:'space-between'}}>
+        <img style={{width: '25%', height: 'auto'}} src={props.item.image} alt={props.item.title}/>
+      <p style={{textAlign:'end', paddingLeft: '2.5rem'}} className='text-xs'>{props.item.title}</p>
       </div>
-      <div className='flex justify-end'>
+      <div style={{display:'flex', justifyContent:'flex-end'}}>
       <input
-      className='bg-slate-100 w-12 rounded-sm ml-2 px-2 h-5 text-xs'
+      style={{marginLeft: '.5rem', paddingLeft: '.5rem', paddingRight: '.5rem', width: '3rem', height:'1.25rem'}}
+      className='bg-slate-100 rounded-sm text-xs'
       type={'number'}
       min={0}
       value={props.item.qty}
       onChange={(e) => {props.changeQuantity(e, props.item)}}
       name='quantity'/>
-      <p className='text-end text-xs ml-2'>${props.item.qty*props.item.price}</p>
+      <p style={{textAlign:'end', marginLeft: '.5rem'}} className='text-xs'>${props.item.qty*props.item.price}</p>
       </div>
     </div>
   )
