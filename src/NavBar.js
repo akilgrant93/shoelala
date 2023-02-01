1131de47c5bc9192170e1c7326859827f231dda3
Akil Grant <akil.grant.93@gmail.com>
2023-02-01 17:54:25 -0500

    tailwind in navbar converted to raw jsx css styles

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,13 +8,13 @@ export default function NavBar() {
 
   return (
     <div>
-    <ul className='shadow-md py-2 justify-end pr-10 items-center' style={{position: 'fixed', top: 0, width: '100%', zIndex: 999, display:'flex', justifyContent:'space-between' ,backgroundColor:'white'}}>
-      <li className='font-semibold mx-10' style={{padding:5}}><Link href="/"><img className='max-h-8' src={'/shoelala.png'} alt={'Shoelala logo'}/></Link></li>
-      <div className='flex'>
-      <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/">ALL</Link></li>
-      <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/jordan"><SiJordan className='mx-1' size={18}/></Link></li>
-      <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/adidas"><SiAdidas className='mx-1' size={18}/></Link></li>
-      <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/nike"><SiNike className='mx-1' size={18}/></Link></li>
+    <ul className='shadow-md' style={{position: 'fixed', top: 0, width: '100%', zIndex: 999, display:'flex', justifyContent:'space-between' ,backgroundColor:'white', alignItems:'center', paddingRight: 40, boxShadow: '0px 0px 9px #000',}}>
+      <li style={{padding:5, marginLeft: 40}}><Link href="/"><img style={{height: 35}} src={'/shoelala.png'} alt={'Shoelala logo'}/></Link></li>
+      <div style={{display:'flex'}}>
+      <li style={{padding:5, fontSize:12}}><Link href="/">ALL</Link></li>
+      <li style={{padding:5, fontSize:12}}><Link href="/jordan"><SiJordan className='mx-1' size={18}/></Link></li>
+      <li style={{padding:5, fontSize:12}}><Link href="/adidas"><SiAdidas className='mx-1' size={18}/></Link></li>
+      <li style={{padding:5, fontSize:12}}><Link href="/nike"><SiNike className='mx-1' size={18}/></Link></li>
       <li className='font-semibold cursor-pointer' onClick={() => {setIsOpen(true)}} style={{padding:5}}><AiOutlineShoppingCart className='mx-1' size={18}/></li>
       {/* <li className='font-semibold' style={{padding:5, fontSize:12}}><Link href="/">ACCOUNT</Link></li> */}
       </div>
